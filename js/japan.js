9746ef4aca14206aec7d3653735f6149cce6023e
SUXEEE <sue@cps.im.dendai.ac.jp>
2016-03-07 18:58:31 +0900

    Import CSV & use CSV.

--- js/japan.js
+++ js/japan.js
@@ -1,10 +1,10 @@
-// var w = 1000;
-// var h = 1000;
+var w = 1000;
+var h = 1000;
 //Create SVG Element
-// var svg = d3.select("body")
-// .append("svg")
-// .attr("width", w)
-// .attr("height",h);
+var svg = d3.select("body")
+.append("svg")
+.attr("width", w)
+.attr("height",h);
 
 var width = 960,
     height = 1160;
@@ -21,64 +21,69 @@ svg.append("circle")
     .attr("fill","red")
     .attr("stroke-width",3)
     .attr("stroke","orange");
-document.write("Hello ! js world!");
-//
+document.write("Hello ! 3D.js world!");
 
-  //  d3.json("../todouhuken.geojson", function(json) {
-  //         var projection,
-  //         path;
-  //
-  //         projection = d3.geo.mercator()
-  //                .scale(2000)
-  //                .center(d3.geo.centroid(json))  // データから中心点を計算
-  //                .translate([width / 2, height / 2]);
-  //
-  //          path = d3.geo.path().projection(projection);
-  //
-  //          svg.selectAll('path')
-  //          .data(json.features)
-  //          .enter()
-  //          .append('path')
-  //          .attr('d', path)
-  //         //  .attr("fill", function(d){
-  //         //      return "hsl(0,0%,80%)";
-  //         //  })
-  //         //  .attr("stroke","hsl(80,100%,0%)" )
-  //         //  .on('mouseover', function(d){
-  //         //  })
-  //         //  .on('click', function(d) {
-  //         //  });
-  //  });
+// 色の範囲を指定
+var color = d3.scale.quantize()
+    .range([
+        "rgb(191,223,255)",
+        "rgb(153,204,255)",
+        "rgb(115,185,253)",
+        "rgb(77,166,255)",
+        "dnnrgb(38,147,255)",
+        "rgb(0,128,255)",
+        "rgb(0,109,217)",
+        "rgb(0,89,178)",
+        "rgb(0,70,140)",
+        "rgb(0,51,102)"
+]);
 
-
-d3.csv("pop2014.csv", function(data) {
-  console.log(data);
-  d3.json("../todouhuken.geojson", function(json) {
-         var projection,
-         path;
-    for(var i=0; i<data.length; i++) {
-      for(var j=0; j<json.features.length; j++) {
-        if( data[i].ken == json.features[j].properties.name_local ) {
-          json.features[j].properties.population = data[i].population;
+d3.csv("../pop2014.csv", function (data){
+  color.domain([
+    d3.min(data, function (d) {
+        return Number(d.value);
+    }),
+    d3.max(data, function (d) {
+        return Number(d.value);
+    })
+]);
+d3.json("../todouhuken.geojson", function(json) {
+  // JSONの座標データとCSVデータを連携
+for (var i = 0; i < data.length; i++) {
+    var dataState = data[i].ken;
+    console.log(data[i].ken);
+    var dataValue = parseFloat(data[i].population);
+    console.log(data[i].population);
+    console.log(dataValue);
+    for (var j = 0; j < json.features.length; j++) {
+        var jsonState = json.features[j].properties.name_local;
+        //console.log(json.features[i].properties);
+        //console.log(data[i].value);
+        if (dataState == jsonState) {
+            json.features[j].properties.value = dataValue;
+            break;
         }
-      }
     }
+}
+       var projection,
+       path;
 
-    projection = d3.geo.mercator()
-           .scale(2000)
-           .center(d3.geo.centroid(json))  // データから中心点を計算
-           .translate([width / 2, height / 2]);
+       projection = d3.geo.mercator()
+              .scale(2000)
+              .center(d3.geo.centroid(json))  // データから中心点を計算
+              .translate([width / 2, height / 2]);
 
-     path = d3.geo.path().projection(projection);
+        path = d3.geo.path().projection(projection);
 
-    svg.selectAll("path")
-      .data(json.features)
-      .enter()
-      .append("path")
-      .attr("d", path)
-      .style("fill", function (d) {
-        var population = d.properties.population;
-        console.log(population);
+        svg.selectAll('path')
+        .data(json.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        //Nagano Ken
+        //console.log(json.features[20].properties);
+        .style("fill", function(dataValue) {
+        //var population = dataValue;
         if( population > 10000000 )
           var c = "darkred";
         else if( population > 5000000 )
@@ -90,32 +95,26 @@ d3.csv("pop2014.csv", function(data) {
         else
           var c = "yellow";
         return c;
-      })
-      .style("stroke", "gray")
-      .style("stroke-width", "0.5px");
-  });
+         })
+         console.log(dataValue);
+
+});
 });
 
-
-
-// //Load Japan topojson
-// d3.json("../json/japan.topojson", function(json) {
-//       //geo data
-// 	    var geodata = json.objects.japan;
-//       //draw(json);
-//       var japan = topojson.feature(json, geodata).features;
-//       //projection method
-//       var projection = d3.geo.mercator()
-//       .center([137, 34])
-//       .translate([w / 2, h / 2])
-//       .scale(1500);
-//       //latitude and longtitude 2 path data settings
-//       var path = d3.geo.path()
-//       .projection(projection);
-//       //latitude and longtitude 2 path data settings
-//       svg.selectAll("path")
-//       .data(japan)
-//       .enter()
-//       .append("path")
-//       .attr("d", path);
-//     });
+  //  d3.json("../todouhuken.geojson", function(json) {
+  //         var projection,
+  //         path;
+   //
+  //         projection = d3.geo.mercator()
+  //                .scale(2000)
+  //                .center(d3.geo.centroid(json))  // データから中心点を計算
+  //                .translate([width / 2, height / 2]);
+   //
+  //          path = d3.geo.path().projection(projection);
+   //
+  //          svg.selectAll('path')
+  //          .data(json.features)
+  //          .enter()
+  //          .append('path')
+  //          .attr('d', path)
+  //  });
